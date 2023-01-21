84531743bf8d86e12bb6f7950e81a5a135bc48c8
Mimi <1119186082@qq.com>
2023-01-21 15:54:54 +0800

    support pjax

--- index.js
+++ index.js
@@ -17,25 +17,12 @@ hexo.extend.filter.register('theme_inject', injects => {
 
   injects.comment.raw('giscus', `
   {% if page.comments %}
-  <div class="comments">
-    <script src="https://giscus.app/client.js"
-        data-repo="${config.repo}" 
-        data-repo-id="${config.repo_id}" 
-        data-category="${config.category}"
-        data-category-id="${config.category_id}"
-        data-mapping="${config.mapping}" 
-        data-reactions-enabled="${config.reactions_enabled}" 
-        data-emit-metadata="${config.emit_metadata}" 
-        data-theme="${config.theme}"
-        data-lang="${config.lang}"
-        crossorigin="${config.crossorigin}"
-        data-input-position="${config.input_position}"
-        data-loading="${config.loading}"
-        async>
-    </script>
+  <div class="comments giscus-container">
   </div>
   {% endif %}
   `);
+
+  injects.bodyEnd.raw('giscus', utils.getFileContent('giscus.njk'));
 
   injects.style.push(utils.getFilePath('giscus.styl'));
 
